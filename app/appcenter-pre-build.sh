5bc82712f2aafc1e177053b6b5857d15da7ff3bc
byldh <byldh@logibros.com>
2021-10-13 12:24:28 +0900

    빌드테스트 + 스토어 배포

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-(echo ""; echo "org.gradle.java.home=C:/Program Files/Android/Android Studio/jre/bin") >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
+#(echo ""; echo "org.gradle.java.home=C:/Program Files/Android/Android Studio/jre/bin") >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
